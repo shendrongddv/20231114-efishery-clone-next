82542a73d0afcd56420ec95b2ca7c4c4420d6568
Kang Shend Rong <shendrongddv@gmail.com>
2023-11-17 04:30:50 +0700

    20231117: BlogSlides

--- app/keen/_examples/vertical.tsx
+++ app/keen/_examples/vertical.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 const Keen_Vertical = () => {
   const [ref] = useKeenSlider<HTMLDivElement>({
@@ -12,9 +12,9 @@ const Keen_Vertical = () => {
       spacing: 10,
     },
     vertical: true,
-  })
+  });
   return (
-    <div ref={ref} className="keen-slider" style={{ height: 300 }}>>
+    <div ref={ref} className="keen-slider" style={{ height: 300 }}>
       <div className="keen-slider__slide number-slide1">1</div>
       <div className="keen-slider__slide number-slide2">2</div>
       <div className="keen-slider__slide number-slide3">3</div>
@@ -22,7 +22,7 @@ const Keen_Vertical = () => {
       <div className="keen-slider__slide number-slide5">5</div>
       <div className="keen-slider__slide number-slide6">6</div>
     </div>
-  )
-}
- 
+  );
+};
+
 export default Keen_Vertical;
